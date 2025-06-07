b878d441dcba0758f517de2d47f99cc2d06ef08c
Vinayak Raut <vinayakraut1309@gmail.com>
2025-06-07 17:19:51 +0530

    html&css code partically completed

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("DevConnector")
+
+//   "login": "vinayak",
+//   "avatar_url": "https://avatars.githubusercontent.com/u/138519?v=4",
+//   "name": "Vinayak Malavade",
+//   "location": "Pune",
+//   "bio": null,
+//   "public_repos": 10,
+//   "followers": 6,
+//   "following": 1,
